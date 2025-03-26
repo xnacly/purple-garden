62a2b1a30b27f29e9d0c679cd891f03691de50ec
xnacly <47723417+xNaCly@users.noreply.github.com>
2025-03-26 13:37:23 +0100

    main: cli parsing via getopt

--- README.md
+++ README.md
@@ -15,18 +15,21 @@
 Currently there isn't much implemented, but you can test purple_garden as follows:
 
 ```sh
-# by default purple_garden fills $PG to be ./examples/hello-world.pg
+# by default purple_garden fills $PG to be ./examples/hello-world.garden
 make
 
 # results in:
-# 
-#   N_LIST(
-#    N_LIST(
-#     N_IDENT[T_IDENT]('println'),
-#     N_ATOM[T_STRING]('Hello World')
-#    )
-#   )
+# [T_DELIMITOR_LEFT]
+# [T_IDENT]('println')
+# [T_STRING]('Hello World')
+# [T_DELIMITOR_RIGHT]
+# N_LIST(
+#  N_LIST(
+#   N_IDENT[T_IDENT]('println'),
+#   N_ATOM[T_STRING]('Hello World')
+#  )
+# )
 
 # provide a custom file to execute
-make PG=examples/ops.pg
+make PG=examples/ops.garden
 ```
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <stdlib.h>
 
 #include "cc.h"
@@ -7,11 +8,72 @@
 #include "parser.h"
 #include "vm.h"
 
-int main(int argc, char **args) {
-  ASSERT(argc >= 2, "Wanted a filename as an argument, not enough arguments")
+#define VERSION "alpha"
+#define TAG "first"
 
-  char *filename = args[1];
-  String input = IO_read_file_to_string(filename);
+typedef struct {
+  // use block allocator before starting a garden, instead of gc; int because
+  // getopt has no bool support
+  int alloc_block;
+  // enable debug logs
+  int debug;
+  int version;
+  // entry garden
+  char *filename;
+} Args;
+
+static const char *options[] = {
+    "alloc-block",
+    "debug",
+    "version",
+};
+
+void usage() {
+  fprintf(stderr, "usage: purple_garden ");
+  size_t len = sizeof(options) / sizeof(char *);
+  for (size_t i = 0; i < len; i++) {
+    fprintf(stderr, "[--%s] ", options[i]);
+  }
+  fprintf(stderr, "<file.garden>\n");
+}
+
+Args Args_parse(int argc, char **argv) {
+  Args a = (Args){0};
+  struct option long_options[] = {{options[0], no_argument, &a.alloc_block, 1},
+                                  {options[1], no_argument, &a.debug, 1},
+                                  {options[2], no_argument, &a.version, 1},
+                                  {0, 0, 0, 0}};
+
+  int opt;
+  while ((opt = getopt_long(argc, argv, "", long_options, NULL)) != -1) {
+    switch (opt) {
+    case 0:
+      break;
+    default:
+      usage();
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (optind < argc) {
+    a.filename = argv[optind];
+  }
+
+  return a;
+}
+
+int main(int argc, char **argv) {
+  Args a = Args_parse(argc, argv);
+  if (a.version) {
+    fprintf(stderr, "purple_garden: %s-%s\n", VERSION, TAG);
+    return EXIT_SUCCESS;
+  }
+  if (a.filename == NULL) {
+    usage();
+    ASSERT(a.filename != NULL,
+           "Wanted a filename as an argument, not enough arguments")
+  }
+  String input = IO_read_file_to_string(a.filename);
 
   Lexer l = Lexer_new(input);
   Parser p = Parser_new(&l);
