bda5b5a547df1d1a470d29bec339213f6f130253
xnacly <47723417+xNaCly@users.noreply.github.com>
2026-01-14 21:40:05 +0100

    opt/bc: make window creation safer

--- src/opt/mod.rs
+++ src/opt/mod.rs
@@ -21,11 +21,11 @@ const WINDOW_SIZE: usize = 3;
 
 /// bytecode optimisations are done in place
 pub fn bc(bc: &mut Vec<Op>) {
-    for i in 0..bc.len() {
-        if let Some(window) = bc.get_mut(i..i + WINDOW_SIZE) {
-            bc::const_add(window);
-            bc::self_move(window);
-        }
+    let len = bc.len();
+    for i in 0..len.saturating_sub(WINDOW_SIZE) {
+        let window = &mut bc[i..i + WINDOW_SIZE];
+        bc::const_add(window);
+        bc::self_move(window);
     }
 
     bc.retain(|op| !matches!(op, Op::Nop))
