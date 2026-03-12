df82970ed35e83b839f74b217e691c830ebfc547
xnacly <47723417+xNaCly@users.noreply.github.com>
2026-03-12 13:54:58 +0100

    std: add metadata and register io

--- src/std/mod.rs
+++ src/std/mod.rs
@@ -1,1 +1,35 @@
+use crate::{ir::ptype::Type, vm::BuiltinFn};
+
 mod io;
+
+pub struct Pkg {
+    name: &'static str,
+    pkgs: &'static [Pkg],
+    fns: &'static [Fn],
+}
+
+pub struct Fn {
+    name: &'static str,
+    ptr: BuiltinFn<'static>,
+    args: &'static [Type],
+    ret: Type,
+}
+
+pub static STD: &[Pkg] = &[Pkg {
+    name: "io",
+    pkgs: &[],
+    fns: &[
+        Fn {
+            name: "println",
+            ptr: crate::std::io::println,
+            args: &[Type::Str],
+            ret: Type::Void,
+        },
+        Fn {
+            name: "print",
+            ptr: crate::std::io::print,
+            args: &[Type::Str],
+            ret: Type::Void,
+        },
+    ],
+}];
