6cd5c2468497c4ed29087ef493ad17f4c94b9615
xnacly <47723417+xNaCly@users.noreply.github.com>
2026-04-20 15:16:53 +0200

    help: support purple-garden intro embed

--- src/help.rs
+++ src/help.rs
@@ -2,6 +2,7 @@ pub fn print_help_by_topic(topic: Option<&str>) -> &str {
     if let Some(topic) = topic {
         match topic {
             "types" => include_str!("../help/types.txt"),
+            "embed" => include_str!("../help/embed.txt"),
             _ => "unknown topic",
         }
     } else {
