3ef5d4a3fb0e8caf90558d6d84841359d10a42c9
xnacly <47723417+xnacly@users.noreply.github.com>
2026-06-04 17:07:19 +0200

    main: remove config trace

--- purple-garden/src/main.rs
+++ purple-garden/src/main.rs
@@ -27,7 +27,6 @@ macro_rules! err {
 
 fn entry() -> Result<(), Box<dyn std::error::Error>> {
     let conf = <config::Config as clap::Parser>::parse();
-    purple_garden_shared::trace!("starting with {:#?}", &conf);
     match conf.version {
         1 => {
             println!(
